d0389c3527a1be3d253f26b3aa87e1d3a9b5c0b1
XN <nyj2007@163.com>
2026-07-25 16:14:04 +0800

    save: error

--- builtins/exceptions/InternalError.h
+++ builtins/exceptions/InternalError.h
@@ -6,6 +6,7 @@
 #include <string>
 
 // 内部错误：代表编译器自身的实现出了 bug
+// 是 exceptions 里唯一没有对应 SL 类的异常，当然更不能在 SL 层捕获
 class InternalError : public SLException {
 public:
     explicit InternalError(const std::string &file_path, const int row, const int col, const std::string &message)
