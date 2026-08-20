2b4190cfa9a4587ee96fb4efc1d2fd3a0e4b936e
XN <nyj2007@163.com>
2026-08-20 15:09:24 +0800

    save: decimal

--- format.py
+++ format.py
@@ -38,7 +38,7 @@ EXCLUDE_DIR_PATTERNS = [
     ".idea",
     ".venv",
     "cmake-build-*",
-    "decimal_only_for_claude",
+    "decimal_for_reference",
 ]
 # 排除的文件名（x_*.h 是内联 X-Macro 头文件，可按需增删）
 EXCLUDE_FILE_PATTERNS = ["x_*.h"]
